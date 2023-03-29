25b3ecb15e49249cfdab77184a9b104c409babed
Andrita Lucian-Gabriel <gabylucian2000@gmail.com>
2023-03-29 10:40:59 +0300

    Corrected structural

--- src/test/java/ToProvidersTest.java
+++
@@ -1,9 +0,0 @@
-import operations.*;
-import org.junit.Test;
-import service.exceptions.*;
-
-public class ToProvidersTest {
-    @Test
-    public void testCurrencyExchange () throws BankAccountException, BankException {
-    }
-}
